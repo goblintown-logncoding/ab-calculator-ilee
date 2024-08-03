9a507509fb79b602d61a9c522f246989b3ed395f
ianlee08 <ileejam123@gmail.com>
2024-08-03 10:38:12 +0900

    feat: added operation functions to buttons

--- src/components/contents.jsx
+++ src/components/contents.jsx
@@ -1,19 +1,42 @@
 import "./contents.css"
+import { useState, useRef } from "react";
+
 
 const Contents = () => {
+    const [operationState, setOperationState] = useState("+");
+    const [resultState, setResultState] = useState()
+    const inputARef = useRef();
+    const inputBRef = useRef();
+
+    const handleCalculation = () => {
+        const valueA = +inputARef.current.value;
+        const valueB = +inputBRef.current.value;
+        console.log(valueA + valueB);
+        if (operationState === "+") {setResultState(valueA + valueB)}
+        if (operationState === "-") {setResultState(valueA - valueB)}
+        if (operationState === "*") {setResultState(valueA * valueB)}
+        if (operationState === "/") {setResultState(valueA / valueB)}
+
+    }
+
     return (
         <div className = "content-container">
             <div>
-                <button>Add</button>
-                <button>Subtract</button>
-                <button>Multiply</button>
-                <button>Divide</button>
+                <button onClick = {() => {setOperationState("+")}}>Add</button>
+                <button onClick = {() => {setOperationState("-")}}>Subtract</button>
+                <button onClick = {() => {setOperationState("*")}}>Multiply</button>
+                <button onClick = {() => {setOperationState("/")}}>Divide</button>
             </div>
             <div className = "input-container">
-                <input /> ? <input /> =
+                <input ref = {inputARef} /> {operationState} <input ref = {inputBRef} /> = {resultState}
             </div>
             <div>
-                <button className = "calculator-container">Calculate</button>
+                <button 
+                    className = "calculator-container"
+                    onClick = {handleCalculation}
+                    
+                    >Calculate
+                </button>
             </div>
         </div>
     )
